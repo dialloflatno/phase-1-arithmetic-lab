e657632e6a28c67f1f29a723d3c3bf6700b0d963
dialloflatno <brahimdiallo23@gmail.com>
2021-10-17 04:00:58 -0400

    commit tree a6609411733e5351c15250706f8a34e9e7ec13b8

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+// Write your code here\
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2 ;
+
+    console.log(multiply);
+
+ let random = Math.ceil (Math.random()*20)
+ 
+    console.log((Number.isInteger(random)));
+    console.log(random);
+
+ let num3 = 4;
+ let num4 = 12;
+
+ let mod = (num3%num4) ;
+
+    console.log(mod);
+
+        const max = Math.max(4,8,12,16,20);
+
+    console.log(max);
